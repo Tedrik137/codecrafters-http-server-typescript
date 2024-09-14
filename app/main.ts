d172c6b7fff8ab1f156448781cf3299d6718252c
Ricardo Akkari <softwareengricky@outlook.com>
2024-09-14 22:02:22 +1000

    gzip compression

--- app/main.ts
+++ app/main.ts
@@ -32,8 +32,8 @@ const server = net.createServer((socket) => {
                     const compressed = gzipSync(term)
                     console.log(compressed)
 
-                    socket.write(`HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: ${compressed.length}\r\n\r\n${compressed}`)
-                }
+                    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: ${compressed.length}\r\n\r\n`);
+                    socket.write(compressed);                }
                 else {
                     socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n`)
                 }
