1c88422d564ce02f2e657b1433dba64fd48807cf
Ricardo Akkari <softwareengricky@outlook.com>
2024-09-14 21:51:06 +1000

    compression headers

--- app/main.ts
+++ app/main.ts
@@ -25,8 +25,8 @@ const server = net.createServer((socket) => {
         else if (path === `/echo/${term}`) {
             const header = req.split('\r\n')[2]
             const [prop, encoding] = header.split(": ")
-            const encodings = encoding.split(', ')
             if (prop === 'Accept-Encoding') {
+                const encodings = encoding.split(', ')
                 if (encodings.includes('gzip')) {
                     socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\n\r\n`)
                 }
