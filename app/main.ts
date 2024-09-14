b3714e721feb5d72168674b5355f85869cfbd0c7
Ricardo Akkari <softwareengricky@outlook.com>
2024-09-14 21:29:40 +1000

    read request body

--- app/main.ts
+++ app/main.ts
@@ -42,7 +42,7 @@ const server = net.createServer((socket) => {
             else if (method === 'POST') {
                 const arr = req.split('\r\n')
                 console.log(arr)
-                const body = req.split('\r\n')[7]
+                const body = req.split('\r\n')[5]
                 console.log(body)
                 try {
                     writeFileSync(absFilePath, body)
