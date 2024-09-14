262dd45d64cb0c4b190bc768ef23285e33b8d481
Ricardo Akkari <softwareengricky@outlook.com>
2024-09-14 16:15:45 +1000

    respond with body

--- app/main.ts
+++ app/main.ts
@@ -9,12 +9,13 @@ const server = net.createServer((socket) => {
         let req = data.toString()
         const path = req.split(' ')[1]
         const term = path.split('/')[2]
-        const userAgent = req.split(' ')[3]
         
         if (path === '/') {
             socket.write('HTTP/1.1 200 OK\r\n\r\n')
         }
         else if (path === '/user-agent') {
+            const userAgentHeader = req.split(' ')[3]
+            const userAgent = userAgentHeader.split(': ')[1]
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
         }
         else if (path === `/echo/${term}`) {
