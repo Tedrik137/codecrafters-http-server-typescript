93cd120165993d1ac7dbc26dd7278892d1e1bc3b
Ricardo Akkari <softwareengricky@outlook.com>
2024-09-15 14:17:10 +1000

    refactor

--- app/main.ts
+++ app/main.ts
@@ -5,41 +5,124 @@ import { gzipSync } from "zlib";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+type Headers = {[key: string]: string}
+
+const readReq = (data: Buffer) => {
+    let req = data.toString()
+    const lines = req.split('\r\n')
+    const [method, path] = lines[0].split(' ')
+    const term = path.split('/')[2]
+    return [method, path, term]
+}
+
+const readHeaders = (data: Buffer) => {
+    let req = data.toString()
+    const lines = req.split('\r\n')
+    const headers = lines.slice(1, lines.length - 1)
+    const headersObj: Headers = {}
+
+    for (const headerStr in headers) {
+        const [header, value] = headerStr.split(': ')
+        headersObj[header] = value
+    }
+
+    return headersObj
+}
+
+const readBody = (data: Buffer) => {
+    let req = data.toString()
+    const lines = req.split('\r\n')
+    const body = lines[lines.length - 1]
+    return body
+}
+
+class HttpResponseBuilder {
+    version: string;
+    status: string;
+    headers: string;
+    body: Buffer;
+
+    constructor() {
+        this.version = 'HTTP/1.1';
+        this.status = ''
+        this.body = Buffer.from('');
+        this.headers = ''
+    }
+
+    buildResponse() {
+        return `${this.version} ${this.status}\r\n${this.headers}\r\n${this.body}`
+    }
+
+    setVersion(version: string) {
+        this.version = version
+        return this
+    }
+
+    setStatus(status: string) {
+        this.status = status
+        return this
+    }
+
+    setHeaders(headers: Headers) {
+        for (const header in headers) {
+            this.headers += `${header}: ${headers[header]}\r\n`
+        }
+        return this
+    }
+
+    setBody(body: Buffer) {
+        this.body = body
+        return this
+    }
+}
+
+const httpResponseBuilder = new HttpResponseBuilder();
+
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
-        let req = data.toString()
-        const method = req.split(' ')[0]
-        const path = req.split(' ')[1]
-        const term = path.split('/')[2]
+        const [method, path, term] = readReq(data)
+        const headers = readHeaders(data)
         
-       
         if (path === '/') {
-            socket.write('HTTP/1.1 200 OK\r\n\r\n')
+            const response = httpResponseBuilder.setStatus('200 OK').buildResponse()
+            socket.write(response)
         }
         else if (path === '/user-agent') {
-            const headers = req.split('\r\n')[2]
-            const userAgent = headers.split(': ')[1]
-            
-            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
+            const userAgent = headers['User-Agent']
+            const response = httpResponseBuilder.setStatus('200 OK')
+            .setHeaders({'Content-Type': 'text/plain', 'Content-Length': `${userAgent.length}`})
+            .setBody(Buffer.from(userAgent))
+            .buildResponse()
+            socket.write(response)
         }
         else if (path === `/echo/${term}`) {
-            const header = req.split('\r\n')[2]
-            const [prop, encoding] = header.split(": ")
-            if (prop === 'Accept-Encoding') {
-                const encodings = encoding.split(', ')
+            const encodingsObj = headers['Accept-Encoding']
+            if (encodingsObj) {
+                const encodings = encodingsObj.split(', ')
                 if (encodings.includes('gzip')) {
                     const compressed = gzipSync(term)
+                    
+                    const httpResponse = httpResponseBuilder.setStatus('200 OK')
+                    .setHeaders({'Content-Type': 'text/plain', 'Content-Encoding': 'gzip', 'Content-Length': `${compressed.length}`})
+                    .buildResponse()
 
-                    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: ${compressed.length}\r\n\r\n`);
+                    socket.write(httpResponse);
                     socket.write(compressed);                
                 }
                 else {
-                    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n`)
+                    const httpResponse = httpResponseBuilder.setStatus('200 OK')
+                    .setHeaders({'Content-Type': 'text/plain'})
+                    .buildResponse()
+                    socket.write(httpResponse)
                 }
             }
             else {
-                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${term.length}\r\n\r\n${term}`) 
+                const httpResponse = httpResponseBuilder.setStatus('200 OK')
+                    .setHeaders({'Content-Type': 'text/plain', 'Content-Length': `${term.length}`})
+                    .setBody(Buffer.from(term))
+                    .buildResponse()
+                socket.write(httpResponse) 
             }
             
         }
@@ -50,26 +133,38 @@ const server = net.createServer((socket) => {
             if (method === 'GET') {
                 try {
                     const buffer = readFileSync(absFilePath);
-                    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${buffer.length}\r\n\r\n${buffer}`)
+                    const httpResponse = httpResponseBuilder.setStatus('200 OK')
+                    .setHeaders({'Content-Type': 'application/octet-stream', 'Content-Length': `${buffer.length}`})
+                    .setBody(buffer)
+                    .buildResponse()
+                    socket.write(httpResponse)
                     
                 }
                 catch (e) {
-                    socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+                    const httpResponse = httpResponseBuilder.setStatus('404 Not Found')
+                    .buildResponse()
+                    socket.write(httpResponse)
                 }
             }
             else if (method === 'POST') {
-                const body = req.split('\r\n')[5]
+                const body = readBody(data)
                 try {
                     writeFileSync(absFilePath, body)
-                    socket.write('HTTP/1.1 201 Created\r\n\r\n')
+                    const httpResponse = httpResponseBuilder.setStatus('201 Created')
+                    .buildResponse()
+                    socket.write(httpResponse)
                 }
                 catch (e) {
-                    socket.write('HTTP/1.1 500 Internal Server Error\r\n\r\n')
+                    const httpResponse = httpResponseBuilder.setStatus('500 Internal Server Error')
+                    .buildResponse()
+                    socket.write(httpResponse)
                 }
             }
         }
         else {
-            socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+            const httpResponse = httpResponseBuilder.setStatus('404 Not Found')
+            .buildResponse()
+            socket.write(httpResponse)
         }
     })
 
